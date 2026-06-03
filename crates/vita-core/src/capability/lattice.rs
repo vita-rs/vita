49a39d6ea3608f88737cf9cf15c6753544fda8c1
Tony Kan <tianchengkan@gmail.com>
2026-06-03 16:40:02 -0700

    test(core): Add unit tests for HasLattice trait

--- crates/vita-core/src/capability/lattice.rs
+++ crates/vita-core/src/capability/lattice.rs
@@ -8,3 +8,27 @@ pub trait HasLattice<V: Scalar> {
     /// Returns the system's lattice.
     fn lattice(&self) -> Lattice<V>;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::units::length::{Angstrom, Length};
+    use crate::units::volume::CubicAngstrom;
+
+    struct Crystal {
+        lattice: Lattice<f64>,
+    }
+    impl HasLattice<f64> for Crystal {
+        fn lattice(&self) -> Lattice<f64> {
+            self.lattice
+        }
+    }
+
+    #[test]
+    fn lattice() {
+        let crystal = Crystal {
+            lattice: Lattice::cubic(Length::<f64, Angstrom>::new(2.0)).unwrap(),
+        };
+        assert_eq!(crystal.lattice().volume::<CubicAngstrom>().value(), 8.0);
+    }
+}
