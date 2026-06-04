c8edf30a41919a1712f058e315453f63a67989d7
Tony Kan <tianchengkan@gmail.com>
2026-06-04 06:14:29 -0700

    feat(vita): Add vita easter egg

--- crates/vita/src/lib.rs
+++ crates/vita/src/lib.rs
@@ -6,3 +6,9 @@ pub use vita_core as core;
 pub mod prelude {
     pub use crate::core::prelude::*;
 }
+
+#[doc(hidden)]
+#[inline]
+pub fn vita() -> &'static str {
+    "What we observe is not nature itself, but nature exposed to our method of questioning."
+}
