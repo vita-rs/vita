44d077469ebbbed4fa7f8a50b6316eec4a9da290
Tony Kan <tianchengkan@gmail.com>
2026-06-16 02:52:56 -0700

    feat(chem): Add Hybridization enum

--- crates/vita-chem/src/lib.rs
+++ crates/vita-chem/src/lib.rs
@@ -1,8 +1,10 @@
 mod bond_order;
 mod capability;
+mod hybridization;
 mod id;
 
 pub use bond_order::BondOrder;
+pub use hybridization::Hybridization;
 pub use id::BondId;
 
 pub use capability::{HasBondOrders, HasBonds};
