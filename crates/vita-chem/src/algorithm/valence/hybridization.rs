68b5bd4376330563a7b120cac5eabd4ac05898f3
Tony Kan <tianchengkan@gmail.com>
2026-06-23 18:03:34 +0800

    test(chem): Add hybridization (electron-domain count) unit tests

--- crates/vita-chem/src/algorithm/valence/hybridization.rs
+++ crates/vita-chem/src/algorithm/valence/hybridization.rs
@@ -40,3 +40,184 @@ pub fn hybridization<M: HasBondOrders + HasElements + HasFormalCharges + HasRadi
         _ => Hybridization::Other,
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{BondId, BondOrder, HasBonds};
+    use vita_core::{Element, HasSites};
+
+    fn s(n: u32) -> SiteId {
+        SiteId::new(n).unwrap()
+    }
+
+    fn b(n: u32) -> BondId {
+        BondId::new(n).unwrap()
+    }
+
+    fn elem(symbol: &str) -> Element {
+        Element::from_symbol(symbol).unwrap()
+    }
+
+    struct Mol {
+        sites: Vec<SiteId>,
+        elements: Vec<Element>,
+        charges: Vec<i8>,
+        radicals: Vec<u8>,
+        bonds: Vec<BondId>,
+        endpoints: Vec<(SiteId, SiteId)>,
+        orders: Vec<BondOrder>,
+    }
+
+    impl HasSites for Mol {
+        fn sites(&self) -> impl Iterator<Item = SiteId> + '_ {
+            self.sites.iter().copied()
+        }
+    }
+
+    impl HasElements for Mol {
+        fn element(&self, site: SiteId) -> Element {
+            let i = self.sites.iter().position(|&s| s == site).unwrap();
+            self.elements[i]
+        }
+    }
+
+    impl HasFormalCharges for Mol {
+        fn formal_charge(&self, site: SiteId) -> i8 {
+            let i = self.sites.iter().position(|&s| s == site).unwrap();
+            self.charges[i]
+        }
+    }
+
+    impl HasRadicalElectrons for Mol {
+        fn radical_electron(&self, site: SiteId) -> u8 {
+            let i = self.sites.iter().position(|&s| s == site).unwrap();
+            self.radicals[i]
+        }
+    }
+
+    impl HasBonds for Mol {
+        fn bonds(&self) -> impl Iterator<Item = BondId> + '_ {
+            self.bonds.iter().copied()
+        }
+
+        fn bond_endpoints(&self, bond: BondId) -> (SiteId, SiteId) {
+            let i = self.bonds.iter().position(|&x| x == bond).unwrap();
+            self.endpoints[i]
+        }
+    }
+
+    impl HasBondOrders for Mol {
+        fn bond_order(&self, bond: BondId) -> BondOrder {
+            let i = self.bonds.iter().position(|&x| x == bond).unwrap();
+            self.orders[i]
+        }
+    }
+
+    fn atom(symbol: &str, charge: i8, radicals: u8, bonds: &[BondOrder]) -> Mol {
+        let mut mol = Mol {
+            sites: vec![s(1)],
+            elements: vec![elem(symbol)],
+            charges: vec![charge],
+            radicals: vec![radicals],
+            bonds: Vec::new(),
+            endpoints: Vec::new(),
+            orders: Vec::new(),
+        };
+        for (i, &order) in bonds.iter().enumerate() {
+            let neighbour = s(i as u32 + 2);
+            mol.sites.push(neighbour);
+            mol.elements.push(elem("H"));
+            mol.charges.push(0);
+            mol.radicals.push(0);
+            mol.bonds.push(b(i as u32 + 1));
+            mol.endpoints.push((s(1), neighbour));
+            mol.orders.push(order);
+        }
+        mol
+    }
+
+    #[test]
+    fn carbon_geometries() {
+        assert_eq!(
+            hybridization(&atom("C", 0, 0, &[BondOrder::Double; 2]), s(1)),
+            Some(Hybridization::Sp),
+        );
+        assert_eq!(
+            hybridization(&atom("C", 1, 0, &[BondOrder::Single; 3]), s(1)),
+            Some(Hybridization::Sp2),
+        );
+        assert_eq!(
+            hybridization(&atom("C", 0, 0, &[BondOrder::Single; 4]), s(1)),
+            Some(Hybridization::Sp3),
+        );
+    }
+
+    #[test]
+    fn lone_pairs_raise_the_count() {
+        assert_eq!(
+            hybridization(&atom("O", 0, 0, &[BondOrder::Single; 2]), s(1)),
+            Some(Hybridization::Sp3),
+        );
+        assert_eq!(
+            hybridization(&atom("N", 0, 0, &[BondOrder::Single; 3]), s(1)),
+            Some(Hybridization::Sp3),
+        );
+        assert_eq!(
+            hybridization(&atom("O", 0, 0, &[BondOrder::Double]), s(1)),
+            Some(Hybridization::Sp2),
+        );
+        assert_eq!(
+            hybridization(&atom("N", 0, 0, &[BondOrder::Triple]), s(1)),
+            Some(Hybridization::Sp),
+        );
+    }
+
+    #[test]
+    fn hypervalent_geometries() {
+        assert_eq!(
+            hybridization(&atom("P", 0, 0, &[BondOrder::Single; 5]), s(1)),
+            Some(Hybridization::Sp3d),
+        );
+        assert_eq!(
+            hybridization(&atom("S", 0, 0, &[BondOrder::Single; 6]), s(1)),
+            Some(Hybridization::Sp3d2),
+        );
+        assert_eq!(
+            hybridization(&atom("I", 0, 0, &[BondOrder::Single; 7]), s(1)),
+            Some(Hybridization::Sp3d3),
+        );
+    }
+
+    #[test]
+    fn zero_or_one_domain_is_s() {
+        assert_eq!(
+            hybridization(&atom("Na", 1, 0, &[]), s(1)),
+            Some(Hybridization::S),
+        );
+        assert_eq!(
+            hybridization(&atom("H", 0, 0, &[BondOrder::Single]), s(1)),
+            Some(Hybridization::S),
+        );
+    }
+
+    #[test]
+    fn eight_domains_is_other() {
+        assert_eq!(
+            hybridization(&atom("Xe", 0, 0, &[BondOrder::Single; 8]), s(1)),
+            Some(Hybridization::Other),
+        );
+    }
+
+    #[test]
+    fn undefined_when_lone_pairs_undefined() {
+        assert_eq!(
+            hybridization(&atom("C", 0, 0, &[BondOrder::Aromatic; 2]), s(1)),
+            None,
+        );
+        assert_eq!(
+            hybridization(&atom("Fe", 0, 0, &[BondOrder::Single; 2]), s(1)),
+            None
+        );
+    }
+}
