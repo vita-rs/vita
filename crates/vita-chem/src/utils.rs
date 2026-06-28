4cb9c21d1cea6efe04890900271e14f74b504961
Tony Kan <tianchengkan@gmail.com>
2026-06-28 09:31:45 +0800

    feat(chem): Add subgraph monomorphism embedding utility

--- crates/vita-chem/src/utils.rs
+++ crates/vita-chem/src/utils.rs
@@ -1,9 +1,11 @@
 mod electronegativity;
+mod embedding;
 mod labelling;
 mod matching;
 mod valence_electrons;
 
 pub use electronegativity::electronegativity;
+pub use embedding::embeddings;
 pub use labelling::labelling;
 pub use matching::maximum_matching;
 pub use valence_electrons::valence_electrons;
